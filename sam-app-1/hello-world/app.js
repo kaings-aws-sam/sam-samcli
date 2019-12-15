aaba3898047a646d1a77e48434561acea5d649fa
willys_tjiawi <willys@huubap.com>
2019-12-15 17:01:17 +0800

    invoke function locally "sam local invoke <FunctionIdentifier> -e <path to event json file>"

--- sam-app-1/hello-world/app.js
+++ sam-app-1/hello-world/app.js
@@ -21,6 +21,7 @@ exports.lambdaHandler = async (event, context) => {
             'statusCode': 200,
             'body': JSON.stringify({
                 message: 'hello world',
+                eventData: event,
                 location: ret.data.trim()
             })
         }
